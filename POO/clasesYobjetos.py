cf5466527979f126ad90cd928f5ce1fdda187145
Nicolas Piñera <nicolaspinera@gmail.com>
2025-04-10 16:50:33 -0300

    POO

---
+++ POO/clasesYobjetos.py
@@ -0,0 +1,8 @@
+class Celular:
+    def __init__(self, marca, modelo, color, precio): #constructor
+        #self hace referencia a la instancia de la clase o sea a si mismo
+        self.marca = marca
+        self.modelo = modelo
+        self.color = color
+        self.precio = precio
+    
